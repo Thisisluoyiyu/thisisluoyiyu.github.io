729b367dc7475435e27466c21b764cd3b4483718
Thisisluoyiyu <Thisisluoyiyu@users.noreply.github.com>
2025-06-16 07:25:20 +0000

    deploy: c8510aea8fed6e93789426273f736ab0b76df3f1

---
+++ assets/back.B5EDAQiO.js
@@ -0,0 +1,1 @@
+import{u as o}from"./chunks/vue-router.ABL-6s2i.js";function t(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{t as u};
